8db9535241012f00f3d443047c7108198cb198a3
Ilham Taufiq <65374686+haiueom@users.noreply.github.com>
2024-02-07 05:06:06 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+destination="$HOME/bin"
+
+[ -d "$destination" ] || mkdir "$destination"
+[ -f "$destination/termux-url-opener" ] && rm -f "$destination/termux-url-opener"
+
+curl -o "$destination/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/main/termux-url-opener"
+chmod +x "$destination/termux-url-opener"
+termux-fix-shebang "$destination/termux-url-opener"
+
+apt update -y
+apt install python ffmpeg -y
+yes | pip3 install -U pip
+yes | pip3 install -U yt-dlp
+
+termux-setup-storage
