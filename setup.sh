b2b15e1a7af8f0987a56ece349ba1c32b8b6042f
haiueom <ilhamtaufiq859@gmail.com>
2024-02-13 14:07:58 +0800

    change branch

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ destination="$HOME/bin"
 [ -d "$destination" ] || mkdir "$destination"
 [ -f "$destination/termux-url-opener" ] && rm -f "$destination/termux-url-opener"
 
-curl -o "$destination/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/main/termux-url-opener"
+curl -o "$destination/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/preview/termux-url-opener"
 chmod +x "$destination/termux-url-opener"
 termux-fix-shebang "$destination/termux-url-opener"
 
