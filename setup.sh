3798e0b13d9d38500d8a477c360b89387dec9733
haiueom <ilhamtaufiq859@gmail.com>
2024-02-19 15:51:27 +0800

    bismillah fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 destination="$HOME/bin"
+config_path="$HOME/.config/yt-dlp"
+default_out_dir="$HOME/storage/downloads/termux"
+
+[ -d "$config_path" ] || mkdir "$config_path"
+[ -f "$config_path/config" ] || rm -f "$config_path/config"
+
+[ -d "$default_out_dir" ] || mkdir "$default_out_dir" ]
 
 [ -d "$destination" ] || mkdir "$destination"
 [ -f "$destination/termux-url-opener" ] && rm -f "$destination/termux-url-opener"
@@ -8,6 +15,8 @@ destination="$HOME/bin"
 curl -o "$destination/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/preview/termux-url-opener"
 chmod +x "$destination/termux-url-opener"
 termux-fix-shebang "$destination/termux-url-opener"
+
+echo "-o $default_out_dir" > "$config_path/config"
 
 apt update -y
 apt install python ffmpeg -y
