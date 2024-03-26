53d1b14e9f9ad881bdf6dfd84ba83d9783ecd00e
haiueom <ilhamtaufiq859@gmail.com>
2024-03-27 02:01:26 +0800

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+echo "Installing termux-url-opener"
 destination="$HOME/bin"
-config_path="$HOME/.config/yt-dlp"
-default_out_dir="$HOME/storage/downloads/termux"
-
-[ -d "$config_path" ] || mkdir "$config_path"
-[ -f "$config_path/config" ] || rm -f "$config_path/config"
-
-[ -d "$default_out_dir" ] || mkdir "$default_out_dir"
-
 [ -d "$destination" ] || mkdir "$destination"
 [ -f "$destination/termux-url-opener" ] && rm -f "$destination/termux-url-opener"
-
 curl -o "$destination/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/main/termux-url-opener"
 chmod +x "$destination/termux-url-opener"
 termux-fix-shebang "$destination/termux-url-opener"
 
-echo "-o $default_out_dir" > "$config_path/config"
+echo "Setup storage"
+termux-setup-storage
 
+echo "Updating termux pkg"
 apt update -y
+
+echo "Installing dependencies"
 apt install python ffmpeg -y
 yes | pip3 install -U pip
 yes | pip3 install -U yt-dlp
 
-termux-setup-storage
+echo "Configuring yt-dlp"
+config_path="$HOME/.config/yt-dlp/config"
+default_out_dir="$HOME/storage/downloads/termux"
+[ -d "$default_out_dir" ] || mkdir -p "$default_out_dir"
+[ -f "$config_path" ] || mkdir -p "$(dirname "$config_path")"
+echo "--output $default_out_dir/%(title)s.%(ext)s" > "$config_path"
 
-echo "Done"
+echo "Done!"
