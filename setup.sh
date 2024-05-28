3a26f039a838ad0a80365869f47f9466d8bdb264
haiueom <ilhamtaufiq859@gmail.com>
2024-05-28 19:51:12 +0800

    chore: Update setup.sh and termux-url-opener scripts
    - Clear the terminal before displaying installation messages
    - Improve formatting and readability of installation messages
    - Update dependencies and installation commands
    - Add option to use cookies for downloading with yt-dlp
    - Add logic to check for existence of cookie files

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 green='\033[1;32m'
 cyan='\033[1;36m'
 white='\033[1;37m'
-red='\033[1;31m'
 reset='\033[0m'
 blue='\033[1;34m'
 bar="$green—————————————————————————$reset"
 
-echo -e "\n${bar}"
-echo -e "${white}[${blue}#${white}] Install Termux URL Opener${reset}"
+clear
+
+echo -e "\n${white}[${blue}#${white}] Install Termux URL Opener${reset}"
 echo -e "${white}[${blue}#${white}] Script by: ${cyan}haiueom${reset}"
 
 echo -e "${bar}"
@@ -18,19 +18,20 @@ echo -e "${white}[${green}+${white}] Cleaning previous installation${reset}"
 [ -f "$HOME/bin/termux-url-opener" ] && rm -f "$HOME/bin/termux-url-opener"
 
 echo -e "${bar}"
-echo -e "${white}[${green}+${white}] Downloading termux-url-opener${reset}"
+echo -e "${white}[${green}+${white}] Downloading termux-url-opener${reset}\n"
 curl -o "$HOME/bin/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/main/termux-url-opener"
 chmod +x "$HOME/bin/termux-url-opener"
 termux-fix-shebang "$HOME/bin/termux-url-opener"
 
-echo -e "${bar}"
-echo -e "${white}[${green}+${white}] Install dependencies${reset}"
-apt update -y && apt upgrade -y
-apt install python ffmpeg wget -y
-yes | pip3 install -U yt-dlp
+echo -e "\n${bar}"
+echo -e "${white}[${green}+${white}] Installing dependencies${reset}\n"
+pkg update -y && pkg upgrade -y
+pkg install python ffmpeg wget -y
+yes | pip install -U --no-deps yt-dlp
 
-echo -e "${bar}"
-echo -e "${white}[${green}+${white}] Done!${reset}"
-echo -e "${white}[${red}!${white}] Don't forget to:${reset}"
-echo -e "${white}[${red}!${white}] \t-> Run 'termux-setup-storage'${reset}"
-echo -e "${white}[${red}!${white}] \t-> Configure yt-dlp config${reset}"
+echo -e "\n${bar}"
+echo -e "${white}[${green}✓${white}] Done!${reset}"
+echo -e "${white}[${blue}!${white}] Don't forget to:${reset}"
+echo -e "${white}[${blue}!${white}]   -> Run 'termux-setup-storage'${reset}"
+echo -e "${white}[${blue}!${white}]   -> Configure yt-dlp config${reset}"
+echo -e "${white}[${blue}!${white}]   -> Add cookie files${reset}"
--- update.sh
+++ update.sh
@@ -7,8 +7,9 @@ reset='\033[0m'
 blue='\033[1;34m'
 bar="$green—————————————————————————$reset"
 
-echo -e "\n${bar}"
-echo -e "${white}[${blue}#${white}] Update Termux URL Opener${reset}"
+clear
+
+echo -e "\n${white}[${blue}#${white}] Update Termux URL Opener${reset}"
 echo -e "${white}[${blue}#${white}] Script by: ${cyan}haiueom${reset}"
 
 echo -e "${bar}"
@@ -16,16 +17,10 @@ echo -e "${white}[${green}+${white}] Cleaning previous installation${reset}"
 [ -f "$HOME/bin/termux-url-opener" ] && rm -f "$HOME/bin/termux-url-opener"
 
 echo -e "${bar}"
-echo -e "${white}[${green}+${white}] Downloading termux-url-opener${reset}"
+echo -e "${white}[${green}+${white}] Downloading termux-url-opener${reset}\n"
 curl -o "$HOME/bin/termux-url-opener" "https://raw.githubusercontent.com/haiueom/termux-url-opener/main/termux-url-opener"
 chmod +x "$HOME/bin/termux-url-opener"
 termux-fix-shebang "$HOME/bin/termux-url-opener"
 
-echo -e "${bar}"
-echo -e "${white}[${green}+${white}] Update dependencies${reset}"
-apt update -y && apt upgrade -y
-apt install python ffmpeg wget -y
-yes | pip3 install -U yt-dlp
-
-echo -e "${bar}"
-echo -e "${white}[${green}+${white}] Done!${reset}"
+echo -e "\n${bar}"
+echo -e "${white}[${green}✓${white}] Done!${reset}"
