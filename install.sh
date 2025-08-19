52ec62bae859d219fa395aefe6c9b41a9df7c57f
Ilham Taufiq <haiueom@gmail.com>
2025-08-19 17:48:27 +0800

    fix shebang line causing command not found

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # --- Configuration ---
 # The name of the main script to be updated.
